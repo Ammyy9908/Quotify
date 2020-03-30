df31f74be6367e4863129b100b415d9404a4e20b
Ammyy9908 <sb78639@gmail.com>
2020-03-30 12:45:54 +0530

    Updates

---
+++ precache-manifest.7802371e4b20e3a96f0a64e12ac2b26d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a584310628047d69ec0e68f5e1b15939",
+    "url": "/quotable/index.html"
+  },
+  {
+    "revision": "a30e4da83afc13706f58",
+    "url": "/quotable/static/js/2.064ffdb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotable/static/js/2.064ffdb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a6d0b0223201f89fbd",
+    "url": "/quotable/static/js/main.97ac23d1.chunk.js"
+  },
+  {
+    "revision": "0e2cb424736c4269b02f",
+    "url": "/quotable/static/js/runtime-main.88f8ebcd.js"
+  },
+  {
+    "revision": "fa5a4493fdd370a9586544d888130d8b",
+    "url": "/quotable/static/media/Devloper.fa5a4493.png"
+  }
+]);
